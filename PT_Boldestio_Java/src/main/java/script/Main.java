7757723c20068ed6ef229455628e8446e733debb
Laptop PabloParapar <Laptop PabloParapar>
2025-05-26 13:21:07 +0200

    Refactor Proyecto Java

--- PT_Boldestio_Java/src/main/java/script/Main.java
+++ PT_Boldestio_Java/src/main/java/script/Main.java
@@ -7,7 +7,6 @@ import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 
 import javax.swing.*;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
